125d247cae27865d8fe523569dae4bea9252012d
AlfalahAnugara <alfalah190299@gmail.com>
2018-12-05 08:32:54 +0700

    membuat Biodata (masih dalam proses)

--- TriangleBoostTubes/src/PPDB_TUBES/BiodataFrame.java
+++ TriangleBoostTubes/src/PPDB_TUBES/BiodataFrame.java
@@ -78,7 +78,8 @@ public class BiodataFrame extends javax.swing.JFrame {
         tanggalComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" }));
         tanggalComboBox.setSelectedIndex(-1);
 
-        BulanComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember", " " }));
+        BulanComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember", "" }));
+        BulanComboBox.setSelectedIndex(-1);
 
         jLabel5.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jLabel5.setText("/");
