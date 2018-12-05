13274644fd3e4fca34be947baf98e2e1f9eff1e4
AlfalahAnugara <alfalah190299@gmail.com>
2018-12-05 08:57:38 +0700

    change variable name

--- TriangleBoostTubes/src/PPDB_TUBES/BiodataFrame.java
+++ TriangleBoostTubes/src/PPDB_TUBES/BiodataFrame.java
@@ -39,7 +39,7 @@ public class BiodataFrame extends javax.swing.JFrame {
     private void initComponents() {
 
         genderGroup = new javax.swing.ButtonGroup();
-        jLabel2 = new javax.swing.JLabel();
+        jLabelNama = new javax.swing.JLabel();
         Namatext = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         Tempatfield = new javax.swing.JTextField();
@@ -47,7 +47,7 @@ public class BiodataFrame extends javax.swing.JFrame {
         BulanComboBox = new javax.swing.JComboBox<>();
         jLabel5 = new javax.swing.JLabel();
         lihatWaktu = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
+        jLabelNISN = new javax.swing.JLabel();
         NISNtext = new javax.swing.JTextField();
         jLabel8 = new javax.swing.JLabel();
         jRadioButton1 = new javax.swing.JRadioButton();
@@ -73,7 +73,7 @@ public class BiodataFrame extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel2.setText("Nama");
+        jLabelNama.setText("Nama");
 
         jLabel1.setText("Tempat,Tanggal Lahir");
 
@@ -96,7 +96,7 @@ public class BiodataFrame extends javax.swing.JFrame {
             }
         });
 
-        jLabel7.setText("NISN");
+        jLabelNISN.setText("NISN");
 
         jLabel8.setText("Jenis Kelamin");
 
@@ -171,8 +171,8 @@ public class BiodataFrame extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel1)
-                    .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 68, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabelNISN, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabelNama, javax.swing.GroupLayout.PREFERRED_SIZE, 68, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel8)
                     .addComponent(jLabel3)
                     .addComponent(jLabel4)
@@ -225,11 +225,11 @@ public class BiodataFrame extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(19, 19, 19)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabelNISN, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(NISNtext, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabelNama, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(Namatext, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -363,14 +363,14 @@ public class BiodataFrame extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
+    private javax.swing.JLabel jLabelNISN;
+    private javax.swing.JLabel jLabelNama;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
