8186bbc137028895d8aca7b1a5e66be9fb558a61
dzikrialifa <dzikrialif99@gmail.com>
2018-12-16 08:45:38 +0700

    Tugas Besar PBO

--- TriangleBoostTubes/src/PPDB_Siswa/PeraturanFrame.java
+++ TriangleBoostTubes/src/PPDB_Siswa/PeraturanFrame.java
@@ -90,6 +90,7 @@ public class PeraturanFrame extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnBackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBackActionPerformed
