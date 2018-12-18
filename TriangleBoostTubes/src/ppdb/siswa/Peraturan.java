7ea848da6199f87250a46b32c432f7e612a050f6
dzikrialifa <dzikrialif99@gmail.com>
2018-12-18 07:50:54 +0700

    Tugas Besar PBO

---
+++ TriangleBoostTubes/src/ppdb/siswa/Peraturan.java
@@ -0,0 +1,22 @@
+
+package ppdb.siswa;
+public class Peraturan {
+    private String nama;
+
+    public Peraturan() {
+    }
+
+    public Peraturan(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+        
+}
