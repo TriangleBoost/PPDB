f3644a2db22763b1d195a0e2b17dab25c4195f98
Ashfin Ramadhandy E <ashfinramadhandy10@gmail.com>
2018-12-18 21:26:38 +0700

    class ortu

---
+++ TriangleBoostTubes/src/ppdb/siswa/Ortu.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ppdb.siswa;
+
+/**
+ *
+ * @author Dhandy
+ */
+public class Ortu {
+    String namaAyah, pekerjaan, gaji, telp, alamat;
+    String namaIbu, pekerjaanIbu, gajiIbu, telpIbu, alamatIbu;
+
+    public Ortu(String namaAyah, String pekerjaan, String gaji, String telp, String alamat, String namaIbu, String pekerjaanIbu, String gajiIbu, String telpIbu, String alamatIbu) {
+        this.namaAyah = namaAyah;
+        this.pekerjaan = pekerjaan;
+        this.gaji = gaji;
+        this.telp = telp;
+        this.alamat = alamat;
+        this.namaIbu = namaIbu;
+        this.pekerjaanIbu = pekerjaanIbu;
+        this.gajiIbu = gajiIbu;
+        this.telpIbu = telpIbu;
+        this.alamatIbu = alamatIbu;
+    }
+
+    public String getNamaAyah() {
+        return namaAyah;
+    }
+
+    public String getPekerjaan() {
+        return pekerjaan;
+    }
+
+    public String getGaji() {
+        return gaji;
+    }
+
+    public String getTelp() {
+        return telp;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public String getNamaIbu() {
+        return namaIbu;
+    }
+
+    public String getPekerjaanIbu() {
+        return pekerjaanIbu;
+    }
+
+    public String getGajiIbu() {
+        return gajiIbu;
+    }
+
+    public String getTelpIbu() {
+        return telpIbu;
+    }
+
+    public String getAlamatIbu() {
+        return alamatIbu;
+    }
+    
+    
+}
