ca5ec014aa42185ccf7905e0e0d13361349f3932
Ashfin Ramadhandy E <ashfinramadhandy10@gmail.com>
2018-12-18 23:13:18 +0700

    menampilkan data yg dimasukan

--- TriangleBoostTubes/src/ppdb/siswa/OutputFrame.java
+++ TriangleBoostTubes/src/ppdb/siswa/OutputFrame.java
@@ -5,7 +5,6 @@
  */
 package ppdb.siswa;
 
-import javax.swing.table.DefaultTableModel;
 public class OutputFrame extends javax.swing.JFrame {
     
     private Biodata bio;
@@ -13,41 +12,47 @@ public class OutputFrame extends javax.swing.JFrame {
     private Nilai nilai;
    
     public OutputFrame() {
-   
         initComponents();
+    }
+    
+    public void setOutput() {
+    // ----------BIODATA-----------------
+        txtNISN.setText(bio.getNisn());
+        txtNama.setText(bio.getNama());
+        String ttl = bio.getTmptLahir() +", "+ bio.getTglLahir();
+        txtTTL.setText(ttl);
+        txtKelamin.setText(bio.getKelamin());
+        txtAgama.setText(bio.getAgama());
+        txtTelp.setText(bio.getTelp());
+        txtAlamat.setText(bio.getAlamat());
+        String anak = "Anak ke "+ bio.getAnak() +" dari "+ bio.getBersaudara() +" Bersudara";
+        txtAnak.setText(anak);
         
-        labelSiswa.setText(bio.getNisn());
-        labelSiswa.setText(bio.getNama());
-        labelSiswa.setText(bio.getTmptLahir());
-        labelSiswa.setText(bio.getTglLahir());
-        labelSiswa.setText(bio.getKelamin());
-        labelSiswa.setText(bio.getAgama());
-        labelSiswa.setText(bio.getTelp());
-        labelSiswa.setText(bio.getAlamat());
-        labelSiswa.setText(bio.getAnak());
-        labelSiswa.setText(bio.getBersaudara());
-        
-        labelOrtu.setText(ortu.getNamaAyah());
-        labelOrtu.setText(ortu.getPekerjaan());
-        labelOrtu.setText(ortu.getGaji());
-        labelOrtu.setText(ortu.getTelp());
-        labelOrtu.setText(ortu.getAlamat());
-        
-        labelOrtu.setText(ortu.getNamaIbu());
-        labelOrtu.setText(ortu.getPekerjaanIbu());
-        labelOrtu.setText(ortu.getGajiIbu());
-        labelOrtu.setText(ortu.getTelpIbu());
-        labelOrtu.setText(ortu.getAlamatIbu());
-        
-        labelNilai.setText(Float.toString(nilai.getMtk()));
-        labelNilai.setText(Float.toString(nilai.getBindo()));
-        labelNilai.setText(Float.toString(nilai.getBing()));
-        labelNilai.setText(Float.toString(nilai.getIpa()));
-        labelNilai.setText(Float.toString(nilai.getRataRata()));
-        
+    //----------ORTU-------------------
+    
+    
+    
+    
+    //----------NILAI------------------
+        nilaiMTK.setText("Matematika : "+ Float.toString(nilai.getMtk()));
+        nilaiBIND.setText("Bahasa Indonesia : "+ Float.toString(nilai.getBindo()));
+        nilaiBING.setText("Bahasa Inggris : "+ Float.toString(nilai.getBing()));
+        nilaiIPA.setText("IPA : "+ Float.toString(nilai.getIpa()));
+        nilaiRATATA.setText("Nilai Rata Rata : "+ Float.toString(nilai.getRataRata()));
     }
 
+    public void setBio(Biodata bio) {
+        this.bio = bio;
+    }
+
+    public void setOrtu(Ortu ortu) {
+        this.ortu = ortu;
+    }
     
+    public void setNilai(Nilai nilai) {
+        this.nilai = nilai;
+    }
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -59,50 +64,216 @@ public class OutputFrame extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
-        labelSiswa = new javax.swing.JLabel();
-        labelOrtu = new javax.swing.JLabel();
-        labelNilai = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        txtNISN = new javax.swing.JLabel();
+        txtNama = new javax.swing.JLabel();
+        txtTTL = new javax.swing.JLabel();
+        txtKelamin = new javax.swing.JLabel();
+        txtAgama = new javax.swing.JLabel();
+        txtTelp = new javax.swing.JLabel();
+        txtAlamat = new javax.swing.JLabel();
+        txtAnak = new javax.swing.JLabel();
+        jPanel3 = new javax.swing.JPanel();
+        jPanel5 = new javax.swing.JPanel();
+        jPanel6 = new javax.swing.JPanel();
+        jPanel4 = new javax.swing.JPanel();
+        nilaiMTK = new javax.swing.JLabel();
+        nilaiBIND = new javax.swing.JLabel();
+        nilaiBING = new javax.swing.JLabel();
+        nilaiIPA = new javax.swing.JLabel();
+        nilaiRATATA = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel2.setText("INFORMASI PENDAFTAR");
 
-        labelSiswa.setText("Biodata Siswa");
+        jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Biodata Siswa", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION));
 
-        labelOrtu.setText("Orang Tua");
+        txtNISN.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        txtNISN.setText("jLabel1");
 
-        labelNilai.setText("Nilai Siswa");
+        txtNama.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        txtNama.setText("jLabel1");
+
+        txtTTL.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        txtTTL.setText("jLabel1");
+
+        txtKelamin.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        txtKelamin.setText("jLabel1");
+
+        txtAgama.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        txtAgama.setText("jLabel1");
+
+        txtTelp.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        txtTelp.setText("jLabel1");
+
+        txtAlamat.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        txtAlamat.setText("jLabel1");
+
+        txtAnak.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        txtAnak.setText("jLabel1");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(txtNISN, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(txtNama, javax.swing.GroupLayout.DEFAULT_SIZE, 238, Short.MAX_VALUE)
+                    .addComponent(txtTTL, javax.swing.GroupLayout.DEFAULT_SIZE, 238, Short.MAX_VALUE)
+                    .addComponent(txtKelamin, javax.swing.GroupLayout.DEFAULT_SIZE, 238, Short.MAX_VALUE)
+                    .addComponent(txtAgama, javax.swing.GroupLayout.DEFAULT_SIZE, 238, Short.MAX_VALUE)
+                    .addComponent(txtTelp, javax.swing.GroupLayout.DEFAULT_SIZE, 238, Short.MAX_VALUE)
+                    .addComponent(txtAlamat, javax.swing.GroupLayout.DEFAULT_SIZE, 238, Short.MAX_VALUE)
+                    .addComponent(txtAnak, javax.swing.GroupLayout.DEFAULT_SIZE, 238, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(txtNISN)
+                .addGap(18, 18, 18)
+                .addComponent(txtNama)
+                .addGap(18, 18, 18)
+                .addComponent(txtTTL)
+                .addGap(18, 18, 18)
+                .addComponent(txtKelamin)
+                .addGap(18, 18, 18)
+                .addComponent(txtAgama)
+                .addGap(18, 18, 18)
+                .addComponent(txtTelp)
+                .addGap(18, 18, 18)
+                .addComponent(txtAlamat)
+                .addGap(18, 18, 18)
+                .addComponent(txtAnak)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jPanel3.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Biodata Siswa", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION));
+
+        jPanel5.setBorder(javax.swing.BorderFactory.createTitledBorder("Ayah"));
+
+        javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
+        jPanel5.setLayout(jPanel5Layout);
+        jPanel5Layout.setHorizontalGroup(
+            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        jPanel5Layout.setVerticalGroup(
+            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 184, Short.MAX_VALUE)
+        );
+
+        jPanel6.setBorder(javax.swing.BorderFactory.createTitledBorder("Ibu"));
+
+        javax.swing.GroupLayout jPanel6Layout = new javax.swing.GroupLayout(jPanel6);
+        jPanel6.setLayout(jPanel6Layout);
+        jPanel6Layout.setHorizontalGroup(
+            jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 258, Short.MAX_VALUE)
+        );
+        jPanel6Layout.setVerticalGroup(
+            jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 184, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addComponent(jPanel5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+
+        jPanel4.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Nilai Siswa", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION));
+
+        nilaiMTK.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        nilaiMTK.setText("Matematika");
+
+        nilaiBIND.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        nilaiBIND.setText("Matematika");
+
+        nilaiBING.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        nilaiBING.setText("Matematika");
+
+        nilaiIPA.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        nilaiIPA.setText("Matematika");
+
+        nilaiRATATA.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        nilaiRATATA.setText("Matematika");
+
+        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
+        jPanel4.setLayout(jPanel4Layout);
+        jPanel4Layout.setHorizontalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel4Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(nilaiMTK, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(nilaiBIND, javax.swing.GroupLayout.DEFAULT_SIZE, 180, Short.MAX_VALUE)
+                    .addComponent(nilaiBING, javax.swing.GroupLayout.DEFAULT_SIZE, 180, Short.MAX_VALUE)
+                    .addComponent(nilaiIPA, javax.swing.GroupLayout.DEFAULT_SIZE, 180, Short.MAX_VALUE)
+                    .addComponent(nilaiRATATA, javax.swing.GroupLayout.DEFAULT_SIZE, 180, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        jPanel4Layout.setVerticalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel4Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(nilaiMTK)
+                .addGap(18, 18, 18)
+                .addComponent(nilaiBIND)
+                .addGap(18, 18, 18)
+                .addComponent(nilaiBING)
+                .addGap(18, 18, 18)
+                .addComponent(nilaiIPA)
+                .addGap(18, 18, 18)
+                .addComponent(nilaiRATATA)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 547, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(30, Short.MAX_VALUE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(labelSiswa)
-                        .addGap(44, 44, 44)
-                        .addComponent(labelOrtu)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(labelNilai)
-                        .addGap(96, 96, 96))))
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(68, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(labelSiswa)
-                    .addComponent(labelOrtu)
-                    .addComponent(labelNilai))
-                .addGap(228, 228, 228))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -116,7 +287,9 @@ public class OutputFrame extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 11, Short.MAX_VALUE))
         );
 
         pack();
@@ -163,8 +336,23 @@ public class OutputFrame extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JLabel labelNilai;
-    private javax.swing.JLabel labelOrtu;
-    private javax.swing.JLabel labelSiswa;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JPanel jPanel5;
+    private javax.swing.JPanel jPanel6;
+    private javax.swing.JLabel nilaiBIND;
+    private javax.swing.JLabel nilaiBING;
+    private javax.swing.JLabel nilaiIPA;
+    private javax.swing.JLabel nilaiMTK;
+    private javax.swing.JLabel nilaiRATATA;
+    private javax.swing.JLabel txtAgama;
+    private javax.swing.JLabel txtAlamat;
+    private javax.swing.JLabel txtAnak;
+    private javax.swing.JLabel txtKelamin;
+    private javax.swing.JLabel txtNISN;
+    private javax.swing.JLabel txtNama;
+    private javax.swing.JLabel txtTTL;
+    private javax.swing.JLabel txtTelp;
     // End of variables declaration//GEN-END:variables
 }
