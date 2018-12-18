4cc903ff9779db648d2774e13fa0d4237a665db3
dzikrialifa <dzikrialif99@gmail.com>
2018-12-18 07:49:07 +0700

    Tugas Besar PBO

---
+++ TriangleBoostTubes/src/ppdb/view/viewSiswa.java
@@ -0,0 +1,62 @@
+/*
+*   DZIKRI ALIF AB
+*   Email : dzikrialif99@gmail.com
+*   Hawlet Packard
+*/
+package ppdb.view;
+public class viewSiswa {
+    
+    private String nama,nisn,telp,alamat,jenisKelamin,agama;
+    
+    public viewSiswa() {
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNisn() {
+        return nisn;
+    }
+
+    public void setNisn(String nisn) {
+        this.nisn = nisn;
+    }
+
+    public String getTelp() {
+        return telp;
+    }
+
+    public void setTelp(String telp) {
+        this.telp = telp;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public String getAgama() {
+        return agama;
+    }
+
+    public void setAgama(String agama) {
+        this.agama = agama;
+    }
+    
+}
