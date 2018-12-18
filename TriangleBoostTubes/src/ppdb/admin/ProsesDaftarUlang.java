510a763cc25ab15f33d85add36d98f0c3dccd0e2
AlfalahAnugara <alfalah190299@gmail.com>
2018-12-18 15:40:30 +0700

    Pengeditan Frame ProsesDaftarUlang.Frame

--- TriangleBoostTubes/src/ppdb/admin/ProsesDaftarUlang.java
+++ TriangleBoostTubes/src/ppdb/admin/ProsesDaftarUlang.java
@@ -32,6 +32,11 @@ public class ProsesDaftarUlang extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jButton1.setText("Back");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -52,6 +57,12 @@ public class ProsesDaftarUlang extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        IndexAdmin IA = new IndexAdmin();
+        IA.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
