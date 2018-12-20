9ad5c23caa15f118a6f273a3ca966bac6515cad3
AlfalahAnugara <alfalah190299@gmail.com>
2018-12-20 18:53:22 +0700

    Bismillah , penambahan kelas output

---
+++ TriangleBoostTubes/src/ppdb/siswa/Output.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ppdb.siswa;
+
+/**
+ *
+ * @author MSI Notebook
+ */
+public class Output {
+    
+    private Biodata biodata ;
+    private Nilai nilai ;
+    private Ortu ortu ;
+
+    public Output() {
+    }
+
+    public Biodata getBiodata() {
+        return biodata;
+    }
+
+    public void setBiodata(Biodata biodata) {
+        this.biodata = biodata;
+    }
+
+    public Nilai getNilai() {
+        return nilai;
+    }
+
+    public void setNilai(Nilai nilai) {
+        this.nilai = nilai;
+    }
+
+    public Ortu getOrtu() {
+        return ortu;
+    }
+
+    public void setOrtu(Ortu ortu) {
+        this.ortu = ortu;
+    }
+    
+    
+}
+
+    
