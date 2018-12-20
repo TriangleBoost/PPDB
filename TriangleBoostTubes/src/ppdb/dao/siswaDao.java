0d735067ed6e6c359d57665b6afcfc48e1e5a15a
AlfalahAnugara <alfalah190299@gmail.com>
2018-12-20 19:03:28 +0700

    Bismillah , perbaikan kelas siswaDao

--- TriangleBoostTubes/src/ppdb/dao/siswaDao.java
+++ TriangleBoostTubes/src/ppdb/dao/siswaDao.java
@@ -22,11 +22,11 @@ import ppdb.view.viewSiswa;
 public class siswaDao implements implementSiswa {
     Connection connect;
     
-    final String insert = "Insert into siswa (NISN , Nama , Tempat , Tanggal_lahir , JenisKelamin , Agama , No_telp , Alamat , Anak_ke, Saudara) VALUES (?,?,?,?,?,?,?,?,?,?) ";
-    final String update = "Update siswa set NISN=? , Nama=? , Tempat=? , Tanggal_lahir=? , Agama=? , No_telp=? , Alamat=? , Anak_ke=? : ";
-    final String delete = "Delete from siswa where NISN=? : ";
+    final String insert = "Insert into siswa (nisn , nama , tempat , tanggal_lahir , jenis_kelamin , agama , no_telp , alamat) VALUES (?,?,?,?,?,?,?,?) ";
+    final String update = "Update siswa set NISN=? , Nama=? , Tempat=? , Tanggal_lahir=? , Agama=? , No_telp=? , alamat=? : ";
+    final String delete = "Delete from siswa where id_siswa=? : ";
     final String select = "select * from siswa";
-    final String carinama = "select * from siswa where nama like ? :";
+    final String carinama = "select * from siswa where id_siswa like ? :";
     
     public siswaDao() {
         connect = Koneksi.connection();
@@ -44,8 +44,6 @@ public class siswaDao implements implementSiswa {
            statement.setString(6, bio.getKelamin());
            statement.setString(7, bio.getTelp());
            statement.setString(8, bio.getAlamat());
-           statement.setString(9, bio.getAnak());
-           statement.setString(10, bio.getBersaudara());
            int i = statement.executeUpdate();
            if(i == 1) {
                return true;
